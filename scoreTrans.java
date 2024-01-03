541d8781ed14c20a1ff8355f78f8c937e0863289
zoezou9 <zoezou9@gmail.com>
2024-01-03 18:13:59 +0800

    add main function

--- scoreTrans.java
+++ scoreTrans.java
@@ -1,5 +1,6 @@
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class scoreTrans {
     public static char letterGrade(int score) {
@@ -14,16 +15,19 @@ public class scoreTrans {
     }
 
     public static void main(String[] args) {
-        int score = 0;
-        Scanner input = new Scanner(System.in);
+        int score = 0; // Declare the variable score
+        System.out.print("Enetr the score = ");
         try {
-            System.out.print("Enter a score: ");
-            score = input.nextInt();
-            System.out.println("The grade is " + letterGrade(score));
-        } finally {
-            input.close();
-        }
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(isr);
+            score = Integer.parseInt(br.readLine());
+            char grade = letterGrade(score);
+            System.out.println("The grade of " + score + " is " + grade);
+        } catch (NumberFormatException ex){
+            System.out.println("Not an integer!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }       
     }
-
 }
 
--- scoreTransTest.java
+++ scoreTransTest.java
@@ -3,77 +3,32 @@ import static org.junit.Assert.*;
 
 public class scoreTransTest {
     @Test
-    public void testGradeA() {
+    public void testGradeBoundary() {
         letterGrade s = new letterGrade();
         assertEquals('A', s.letterGrade(90));
         assertEquals('A', s.letterGrade(100));
-    }
-
-    @Test
-    public void testGradeB() {
-        letterGrade s = new letterGrade();
         assertEquals('B', s.letterGrade(80));
         assertEquals('B', s.letterGrade(89));
-    }
-
-    @Test
-    public void testGradeC() {
-        letterGrade s = new letterGrade();
         assertEquals('C', s.letterGrade(70));
         assertEquals('C', s.letterGrade(79));
-    }
-
-    @Test
-    public void testGradeD() {
-        letterGrade s = new letterGrade();
         assertEquals('D', s.letterGrade(60));
         assertEquals('D', s.letterGrade(69));
-    }
-
-    @Test
-    public void testGradeF() {
-        letterGrade s = new letterGrade();
         assertEquals('F', s.letterGrade(0));
         assertEquals('F', s.letterGrade(59));
-    }
-
-    @Test
-    public void testGradeX() {
-        // scoreTrans s = new scoreTrans();
-        letterGrade s = new letterGrade();
         assertEquals('X', s.letterGrade(101));
         assertEquals('X', s.letterGrade(-1));
     }
 
-    // @Test
-    // public void testGradeA() {
-    //     assertEquals('A', scoreTrans.letterGrade(95));
-    // }
+    @Test
+    public void testGrade() {
+        assertEquals('A', scoreTrans.letterGrade(95));
+        assertEquals('B', scoreTrans.letterGrade(85));
+        assertEquals('C', scoreTrans.letterGrade(75));
+        assertEquals('D', scoreTrans.letterGrade(65));
+        assertEquals('F', scoreTrans.letterGrade(55));
+        assertEquals('X', scoreTrans.letterGrade(105));
+        assertEquals('X', scoreTrans.letterGrade(-5));
+    }
 
-    // @Test
-    // public void testGradeB() {
-    //     assertEquals('B', scoreTrans.letterGrade(85));
-    // }
-
-    // @Test
-    // public void testGradeC() {
-    //     assertEquals('C', scoreTrans.letterGrade(75));
-    // }
-
-    // @Test
-    // public void testGradeD() {
-    //     assertEquals('D', scoreTrans.letterGrade(65));
-    // }
-
-    // @Test
-    // public void testGradeF() {
-    //     assertEquals('F', scoreTrans.letterGrade(55));
-    // }
-
-    // @Test
-    // public void testGradeX() {
-    //     assertEquals('X', scoreTrans.letterGrade(105));
-    //     assertEquals('X', scoreTrans.letterGrade(-5));
-    // }
 }
 
