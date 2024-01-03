11e7a71dab18cb3e3768d5774342678659ec2d60
zoezou9 <zoezou9@gmail.com>
2024-01-03 23:19:32 +0800

    test refactor

--- scoreTransTest.java
+++ scoreTransTest.java
@@ -4,19 +4,19 @@ import static org.junit.Assert.*;
 public class scoreTransTest {
     @Test
     public void testGradeBoundary() {
-        letterGrade s = new letterGrade();
-        assertEquals('A', s.letterGrade(90));
-        assertEquals('A', s.letterGrade(100));
-        assertEquals('B', s.letterGrade(80));
-        assertEquals('B', s.letterGrade(89));
-        assertEquals('C', s.letterGrade(70));
-        assertEquals('C', s.letterGrade(79));
-        assertEquals('D', s.letterGrade(60));
-        assertEquals('D', s.letterGrade(69));
-        assertEquals('F', s.letterGrade(0));
-        assertEquals('F', s.letterGrade(59));
-        assertEquals('X', s.letterGrade(101));
-        assertEquals('X', s.letterGrade(-1));
+        // letterGrade s = new letterGrade();
+        assertEquals('A', scoreTrans.letterGrade(90));
+        assertEquals('A', scoreTrans.letterGrade(100));
+        assertEquals('B', scoreTrans.letterGrade(80));
+        assertEquals('B', scoreTrans.letterGrade(89));
+        assertEquals('C', scoreTrans.letterGrade(70));
+        assertEquals('C', scoreTrans.letterGrade(79));
+        assertEquals('D', scoreTrans.letterGrade(60));
+        assertEquals('D', scoreTrans.letterGrade(69));
+        assertEquals('F', scoreTrans.letterGrade(0));
+        assertEquals('F', scoreTrans.letterGrade(59));
+        assertEquals('X', scoreTrans.letterGrade(101));
+        assertEquals('X', scoreTrans.letterGrade(-1));
     }
 
     @Test
